d348164c2664bd6608f8c9552f4f94e9f87858c6
Assem Allam <assem.russia123@gmail.com>
2025-07-05 16:32:28 +0300

    .

--- src/test/java/test_Cases/TestBase.java
+++ src/test/java/test_Cases/TestBase.java
@@ -3,17 +3,17 @@ package test_Cases;
 import com.codeborne.selenide.Configuration;
 import static com.codeborne.selenide.Selenide.*;
 
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+//import org.testng.annotations.AfterSuite;
+//import org.testng.annotations.BeforeSuite;
 
 
 public class TestBase {
-	@BeforeSuite
+	//@BeforeSuite
 	public void startBrowser(){
 		Configuration.browser = "firefox";
 		open("https://automationteststore.com/");
 	}
-	@AfterSuite
+	//@AfterSuite
 	public void closeBroswer(){
 	closeWebDriver();
 		
