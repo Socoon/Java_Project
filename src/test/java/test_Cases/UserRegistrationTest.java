66bbedb7863aa7dc27c658716ec1ed0eab1ffe06
Assem Allam <assem.russia123@gmail.com>
2025-07-05 21:15:12 +0300

    .

--- src/test/java/test_Cases/UserRegistrationTest.java
+++ src/test/java/test_Cases/UserRegistrationTest.java
@@ -1,4 +1,6 @@
 package test_Cases;
+import static com.codeborne.selenide.Selenide.open;
+
 import org.testng.annotations.Test;
 
 import sitePages.*;
@@ -8,6 +10,7 @@ public class UserRegistrationTest extends TestBase{
 	UserAccountPage UserAccountPageobject = new UserAccountPage();
 	@Test
 	public void userRegistrationSteps(){
+		open("https://automationteststore.com/");
 		HomePageobject.selectEuroCurrency();
 		HomePageobject.clickRegisterLink();
 		RegisterPageobject.fillUserRegistrationForm("jhonne", "bonny", "jhonne" + System.currentTimeMillis() + "@gmail.com", "123street", "cityname", "12345", "jhon" + System.currentTimeMillis(), "Pass@123456789");
